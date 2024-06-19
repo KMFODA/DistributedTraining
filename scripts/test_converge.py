9d709a0f07bc2da129dc3144fa2775c33e134ecb
mikkel <thybo11@hotmail.com>
2024-06-19 10:04:42 +0200

    ADD: update converge test script

--- scripts/test_converge.py
+++ scripts/test_converge.py
@@ -1,9 +1,9 @@
-import random
+import os
+import numpy as np
 import wandb
 import torch
 import math
 from transformers import AutoModelForCausalLM
-from template.data.dataset import SubsetFalconLoader
 from torch_optimizer import Lamb
 
 # Custom learning rate scheduler function
@@ -17,11 +17,49 @@ def get_lr(it, warmup_iters, lr_decay_iters, learning_rate, min_lr):
     coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
     return min_lr + coeff * (learning_rate - min_lr)
 
-# Initialize wandb
-wandb.init(project="optimizer_ablation_study")
+def load_tokens(filename):
+    npt = np.load(filename)
+    ptt = torch.tensor(npt, dtype=torch.long)
+    return ptt
+
+class DataLoaderLite:
+    def __init__(self, B, T, split):
+        self.B = B
+        self.T = T
+        assert split in {'train', 'val'}
+
+        # get the shard filenames
+        data_root = "edu_fineweb10B"
+        shards = os.listdir(data_root)
+        shards = [s for s in shards if split in s]
+        shards = sorted(shards)
+        shards = [os.path.join(data_root, s) for s in shards]
+        self.shards = shards
+        assert len(shards) > 0, f"no shards found for split {split}"
+        self.reset()
+
+    def reset(self):
+        # state, init at shard zero
+        self.current_shard = 0
+        self.tokens = load_tokens(self.shards[self.current_shard])
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position + B*T + 1]
+        x = (buf[:-1]).view(B, T) # inputs
+        y = (buf[1:]).view(B, T) # targets
+        # advance the position in the tensor
+        self.current_position += B * T
+        # if loading the next batch would be out of bounds, advance to next shard
+        if self.current_position + (B * T + 1) > len(self.tokens):
+            self.current_shard = (self.current_shard + 1) % len(self.shards)
+            self.tokens = load_tokens(self.shards[self.current_shard])
+            self.current_position = 0
+        return x, y
 
 # Function to run training with specified optimizer and learning rate
-def train_model(optimizer_name, learning_rate, global_target_batch_size=32000, num_epochs=1, warmup_proportion=0.1, min_lr=1e-6):
+def train_model(optimizer_name, learning_rate, global_target_batch_size=524_288, num_epochs=1, warmup_proportion=0.1, min_lr=1e-6):
     # Initialize wandb with configuration details
     wandb.init(project="optimizer_ablation_study", config={
         "optimizer": optimizer_name,
@@ -35,7 +73,7 @@ def train_model(optimizer_name, learning_rate, global_target_batch_size=32000, n
     config = wandb.config
 
     # Load the model
-    model = AutoModelForCausalLM.from_pretrained("kmfoda/gpt2-500m")
+    model = AutoModelForCausalLM.from_pretrained("kmfoda/gpt2-200m")
     model.to("cuda")
 
     # Set up the optimizer
@@ -46,9 +84,11 @@ def train_model(optimizer_name, learning_rate, global_target_batch_size=32000, n
     else:
         raise ValueError("Unsupported optimizer")
 
-    BATCH_SIZE = 2    
+    BATCH_SIZE = 1    
+    SEQ_LENGTH = 1024
+    grad_accum_steps = global_target_batch_size // (BATCH_SIZE * SEQ_LENGTH)
 
-    total_steps = 640_000 * num_epochs
+    total_steps = global_target_batch_size * 10 * num_epochs
     warmup_steps = int(warmup_proportion * total_steps)
     lr_decay_iters = total_steps - warmup_steps
 
@@ -58,22 +98,27 @@ def train_model(optimizer_name, learning_rate, global_target_batch_size=32000, n
     lr = get_lr(step, warmup_steps, lr_decay_iters, learning_rate, min_lr)
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
+        
+    #train_loader = DataLoaderLite(B=B, T=T, process_rank=0, num_processes=1, split="train")
 
-    while current_step < total_steps:
+    for current_step in range(total_steps):
         dataloader = SubsetFalconLoader(
         batch_size=BATCH_SIZE,
-        sequence_length=1024,
+        sequence_length=SEQ_LENGTH,
         rows=random.choices(range(0, 519_000_000), k=1000),
         )
 
-        for i, batch in enumerate(dataloader):
-            inputs = batch.to("cuda")
+    
+        for micro_step in range(grad_accum_steps):
+            x, y = train_loader.next_batch()
+            inputs = x.to("cuda")
+            targets = y.to("cuda")
 
             # Forward pass
             outputs = model(input_ids=inputs, labels=inputs)
 
             loss = outputs.loss
-            scaled_loss = loss / global_target_batch_size / BATCH_SIZE
+            scaled_loss = loss / grad_accum_steps
             print(f"Current step {current_step+1}, Batch {i+1}, Loss: {loss.item()}")
             scaled_loss.backward()
 
@@ -81,34 +126,43 @@ def train_model(optimizer_name, learning_rate, global_target_batch_size=32000, n
             step += BATCH_SIZE
             current_step += BATCH_SIZE
 
-            if local_samples >= global_target_batch_size:
-                lr = get_lr(step, warmup_steps, 
-                            lr_decay_iters, learning_rate, min_lr)
-                for param_group in optimizer.param_groups:
-                    param_group['lr'] = lr
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
 
-                optimizer.step()
-                optimizer.zero_grad()  # Reset gradients after each step
-                local_samples = 0
+        lr = get_lr(step, warmup_steps, 
+                    lr_decay_iters, learning_rate, min_lr)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
 
-            # Log metrics to wandb
-            wandb.log({
-                "loss": loss.item(),
-                "current step": current_step+1,
-                "batch": i+1,
-                "learning_rate": lr,
-                "optimizer": optimizer_name,
-                "learning_rate_config": learning_rate
-            })
-            
+        optimizer.step()
+        optimizer.zero_grad()  # Reset gradients after each step
+        local_samples = 0
+        
+        # TODO - implement on chain
+        # tokens_processed = train_loader.B * train_loader.T * grad_accum_steps * ddp_world_size
+        # tokens_per_sec = tokens_processed / dt
+
+        # Log metrics to wandb
+        wandb.log({
+            "loss": loss.item(),
+            "current step": current_step+1,
+            "batch": i+1,
+            "learning_rate": lr,
+            "optimizer": optimizer_name,
+            "learning_rate_config": learning_rate
+        })
+        
     # Finish the wandb run
     wandb.finish()
 
+
+# Initialize wandb
+wandb.init(project="optimizer_ablation_study")
+
 # Define the grid search parameters
 optimizers = ["AdamW", "LAMB"]
-learning_rates = [5e-3, 5e-2, 1e-3]
+learning_rates = [5e-3, 5e-2, 5e-4]
 
 # Run the grid search
 for optimizer_name in optimizers:
     for lr in learning_rates:
-        train_model(optimizer_name, lr)
+        train_model(optimizer_name, lr, min_lr=lr*0.1)
