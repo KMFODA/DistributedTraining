dc05c55dfae814a40f0018458bc27863bb5d56ef
kmfoda <kmfoda@gmail.com>
2024-07-24 16:29:31 +0000

    Fix setup.py error
    Rename custom_proto folder to proto

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # The MIT License (MIT)
 # Copyright © 2023 Yuma Rao
-# TODO(developer): Set your name
+# KMFODA
 # Copyright © 2023 <your name>
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
@@ -19,55 +19,13 @@
 
 import codecs
 import os
-import glob
-
-
 import re
 from io import open
 from os import path
-import subprocess
 
 from setuptools import find_packages, setup
-from setuptools.command.install import install
-from setuptools.command.build_py import build_py
 
 
-def custom_proto_compile(output_path):
-    print("Compiling custom proto files...")
-    import grpc_tools.protoc
-    
-    proto_path = os.path.join(os.path.dirname(__file__), "template", "custom_proto")
-    proto_files = glob.glob(os.path.join(proto_path, "*.proto"))
-    
-    if not proto_files:
-        print("No .proto files found in", proto_path)
-        return
-    
-    cli_args = [
-        "grpc_tools.protoc",
-        f"--proto_path={proto_path}",
-        f"--python_out={output_path}",
-        f"--grpc_python_out={output_path}",
-    ] + proto_files
-    
-    print(f"Custom proto files found: {proto_files}")
-    
-    code = grpc_tools.protoc.main(cli_args)
-    if code:
-        raise ValueError(f"{' '.join(cli_args)} finished with exit code {code}")
-    
-    # Make pb2 imports in generated scripts relative
-    for script in glob.iglob(os.path.join(output_path, "*_pb2*.py")):
-        with open(script, "r+") as file:
-            code = file.read()
-            file.seek(0)
-            file.write(re.sub(r"\n(import .+_pb2.*)", "from . \\1", code))
-            file.truncate()
-    
-    print("Custom proto compilation completed.")
-
-
-                
 def read_requirements(path):
     with open(path, "r") as f:
         requirements = f.read().splitlines()
@@ -103,41 +61,73 @@ with codecs.open(
     )
     version_string = version_match.group(1)
 
-class CustomInstallCommand(install):
+from setuptools.command.build_py import build_py
+from setuptools.command.develop import develop
+import glob
+
+
+def proto_compile(output_path):
+    import grpc_tools.protoc
+
+    cli_args = [
+        "grpc_tools.protoc",
+        "--proto_path=template/proto",
+        f"--python_out={output_path}",
+        f"--grpc_python_out={output_path}",
+    ] + glob.glob("template/proto/*.proto")
+
+    code = grpc_tools.protoc.main(cli_args)
+    if (
+        code
+    ):  # hint: if you get this error in jupyter, run in console for richer error message
+        raise ValueError(f"{' '.join(cli_args)} finished with exit code {code}")
+    # Make pb2 imports in generated scripts relative
+    for script in glob.iglob(f"{output_path}/*.py"):
+        with open(script, "r+") as file:
+            code = file.read()
+            file.seek(0)
+            file.write(re.sub(r"\n(import .+_pb2.*)", "from . \\1", code))
+            file.truncate()
+
+
+class BuildPy(build_py):
+    user_options = build_py.user_options + [
+        ("buildgo", None, "Builds p2pd from source")
+    ]
+
+    def initialize_options(self):
+        super().initialize_options()
+        self.buildgo = False
+
     def run(self):
-        print("Running CustomInstallCommand")
-        self.run_command('egg_info')
-        subprocess.check_call(['pip', 'install', '-r', 'requirements.txt'])
-        subprocess.check_call(['pip', 'install', 'grpcio-tools'])
-        print("About to call custom_proto_compile")
-        custom_proto_compile("template/custom_proto")
-        print("Finished custom_proto_compile")
-        install.run(self)
-        
-class CustomBuildCommand(build_py):
+        super().run()
+        print(os.path.join(os.path.dirname(__file__), "template", "proto"))
+        proto_compile(os.path.join(os.path.dirname(__file__), "template", "proto"))
+
+
+class Develop(develop):
     def run(self):
-        print("Running CustomBuildCommand")
-        custom_proto_compile("template/custom_proto")
-        build_py.run(self)
+        self.reinitialize_command("build_py", build_lib=here)
+        self.run_command("build_py")
+        super().run()
+
 
 setup(
-    name="bittensor_subnet_template",  # TODO(developer): Change this value to your module subnet name.
+    name="distributed_training",
     version=version_string,
-    description="bittensor_subnet_template",  # TODO(developer): Change this value to your module subnet description.
+    cmdclass={"build_py": BuildPy, "develop": Develop},
+    description="distributed_training",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/opentensor/bittensor-subnet-template",  # TODO(developer): Change this url to your module subnet github url.
-    author="bittensor.com",  # TODO(developer): Change this value to your module subnet author name.
+    url="https://github.com/KMFODA/DistributedTraining",
+    author="KMFODA",
     packages=find_packages(),
     include_package_data=True,
-    author_email="",  # TODO(developer): Change this value to your module subnet author email.
+    author_email="",
     license="MIT",
     python_requires=">=3.8",
-    install_requires=requirements + ['grpcio-tools'],  # Add grpcio-tools to requirements
-    cmdclass={
-        'install': CustomInstallCommand,
-        'build_py': CustomBuildCommand,
-    },
+    setup_requires=["grpcio-tools"],
+    install_requires=requirements,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
