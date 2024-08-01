391b3850c81e61455f583ebe515c294010ed55c6
kmfoda <kmfoda@gmail.com>
2024-08-01 22:00:38 +0000

    Fix self reference

--- template/validator/reward.py
+++ template/validator/reward.py
@@ -218,7 +218,7 @@ async def get_rewards(
         # Periodically check if peer is connected to DHT & run_id and blacklist them if they are not
         if (self.step % 10) == 0:
             # Update mapping of uids to peerids
-            self.uids_to_peerids = self.map_uid_to_peerid(
+            self.uids_to_peerids = await map_uid_to_peerid(
                 self, range(0, self.metagraph.n)
             )
             self.uids_to_peerids[self.uid] = self.dht.peer_id
