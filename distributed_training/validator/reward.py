Fix gradient averaging logs

distributed_training/validator/reward.py
@@ -25,9 +25,6 @@ from distributed_training.utils.uids import get_random_uids, map_uid_to_peerid
 import time
 import asyncio
 
-torch.use_deterministic_algorithms(True)
-torch.manual_seed(42)
-
 
 def score_gradients(self, response, uid):
     # Create Dataloader
@@ -140,22 +137,22 @@ async def score_bandwidth(self, uids, timeout=120):
     return scores
 
 
-def score_failed_senders(self, uids, failed_senders, participating_peers):
+def score_failed_senders(self, uids, failed_peers, participating_peers):
     scores = torch.FloatTensor([0.0 for _ in uids]).to(self.device)
     for i, uid in enumerate(uids):
         peer_id = self.uids_to_peerids.get(uid)
 
         if peer_id in participating_peers:
-            if peer_id in failed_senders:
-                bt.logging.info(f"- Scoring UID {uid} 0.0 (Failed sender)")
+            if peer_id in failed_peers:
+                bt.logging.info(f"Scoring UID {uid} 0.0 - Failed sender")
                 scores[i] = 0.0
             else:
                 bt.logging.info(
-                    f"- Scoring UID {uid} 1.0 (Successful participating peer)"
+                    f"Scoring UID {uid} 1.0 - Successful participating peer)"
                 )
                 scores[i] = 1.0
         else:
-            bt.logging.info(f"- Scoring UID {uid} 0.0 (Not a participating peer)")
+            bt.logging.info(f"Scoring UID {uid} - Non participating peer")
             scores[i] = 0.0
 
     return scores
@@ -166,7 +163,7 @@ async def get_rewards(
     uids: List[int],
     responses: list,
     all_reduce: bool,
-    failed_senders=None,
+    failed_peers=None,
     participating_peers=None,
 ) -> torch.FloatTensor:
     """
@@ -183,11 +180,10 @@ async def get_rewards(
     """
     # Score a non-empty AllReduce response
     if all_reduce and ((responses != [[]]) or (self.uid != self.master_uid)):
-        # Now that we've called all_reduce on all available UIDs, only score a sample of them to spread
-        # the scoring burden across all validators
-        self.miner_uids = await get_random_uids(
-            self, dendrite=self.dendrite, k=self.config.neuron.sample_size
-        )
+        if self.uid != self.master_uid:
+            # Now that we've called all_reduce on all available UIDs, only score a sample of them to spread
+            # the scoring burden across all validators
+            self.miner_uids = await get_random_uids(self, dendrite=self.dendrite, k=2)
 
         # Set up the scores tensor
         scores = torch.FloatTensor([1 for _ in self.miner_uids]).to(self.device)
@@ -211,7 +207,7 @@ async def get_rewards(
         if self.uid == self.master_uid:
             # Apply penalty to failed senders if any
             failed_sender_scores = score_failed_senders(
-                self, self.miner_uids.tolist(), failed_senders, participating_peers
+                self, self.miner_uids.tolist(), failed_peers, participating_peers
             )
             bt.logging.info(f"Failed Sender Scores: {failed_sender_scores}")
             self.event.update(

neurons/validator.py
@@ -193,7 +193,7 @@ class Validator(BaseValidatorNeuron):
             compression=hivemind.Uniform8BitQuantization(),
             accumulate_grads_on=torch.device("cuda"),
             start=True,
-            min_group_size=2,
+            min_group_size=5,
             min_matchmaking_time=30.0,
             request_timeout=10.0,
             next_chunk_timeout=45.0,
@@ -236,65 +236,6 @@ class Validator(BaseValidatorNeuron):
                 self.local_progress.samples_accumulated += len(response.dataset_indices)
             else:
                 continue
-
-    def map_uids_to_peerids(self):
-        # Track how recently we updated each uid
-        uid_last_checked = dict()
-
-        # The below loop iterates across all miner uids and checks to see
-        # if they should be updated.
-        while not self.stop_event.is_set():
-            try:
-                # Get the next uid to check
-                next_uid = next(self.miner_iterator)
-
-                # Confirm that we haven't checked it in the last 5 minutes.
-                time_diff = (
-                    dt.datetime.now() - uid_last_checked[next_uid]
-                    if next_uid in uid_last_checked
-                    else None
-                )
-
-                if time_diff and time_diff < dt.timedelta(minutes=5):
-                    # If we have seen it within 5 minutes then sleep until it has been at least 5 minutes.
-                    time_to_sleep = (
-                        dt.timedelta(minutes=5) - time_diff
-                    ).total_seconds()
-                    bt.logging.trace(
-                        f"Update loop has already processed all UIDs in the last 5 minutes. Sleeping {time_to_sleep} seconds."
-                    )
-                    time.sleep(time_to_sleep)
-
-                uid_last_checked[next_uid] = dt.datetime.now()
-
-                # Get their hotkey from the metagraph.
-                hotkey = self.metagraph.hotkeys[next_uid]
-
-                # Compare metadata and tracker, syncing new model from remote store to local if necessary.
-                metadata = bt.extrinsics.serving.get_metadata(
-                    self.subtensor, self.config.netuid, self.metagraph.hotkeys[next_uid]
-                )
-
-                if not metadata:
-                    updated = None
-                else:
-                    commitment = metadata["info"]["fields"][0]
-                    hex_data = commitment[list(commitment.keys())[0]][2:]
-                    chain_str = bytes.fromhex(hex_data).decode()
-                    updated = PeerID(chain_str)
-
-                if self.uids_to_peerids[next_uid] != updated:
-                    bt.logging.trace(
-                        f"Updated peerID for UID={next_uid}. Was new = {updated}"
-                    )
-                    self.uids_to_peerids[next_uid] = updated
-
-            except Exception as e:
-                bt.logging.error(
-                    f"Error in update loop: {e} \n {traceback.format_exc()}"
-                )
-
-        bt.logging.info("Exiting update models loop.")
 
     def get_learning_rate(self):
         learning_rate_minimum = self.learning_rate_maximum * 0.1