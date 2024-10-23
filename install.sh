2b2272159867db35f64cad3cd56ad0ac91dbb44d
mikkelmedm <thybo11@hotmail.com>
2024-10-23 12:12:13 +0000

    add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/learning-at-home/hivemind.git
+cd hivemind
+git checkout release/1.1.x
+pip install .
+cd ..
+pip install bittensor
+pip install --upgrade py-multihash
+pip install -e .
