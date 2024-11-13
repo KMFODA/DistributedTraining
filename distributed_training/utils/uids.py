67a24579e1ae60b9761c4e2529c8aa738d03e8ea
kmfoda <kmfoda@gmail.com>
2024-11-13 10:32:24 +0000

    Split check_uid calls into groups of 25

--- distributed_training/utils/uids.py
+++ distributed_training/utils/uids.py
@@ -83,21 +83,28 @@ async def get_random_uids(
     """
     candidate_uids = []
     avail_uids = []
+    uids = [i for i in range(self.metagraph.n)]
+    random.shuffle(uids)
 
-    tasks = []
-    for uid in range(self.metagraph.n.item()):
-        # The dendrite client queries the network.
-        tasks.append(
-            check_uid_availability(
-                dendrite,
-                self.metagraph,
-                uid,
-                self.config.neuron.vpermit_tao_limit,
-                epoch,
+    responses = []
+    attempt = 0
+    limit = 25
+    while (sum(responses) < k) and (attempt < (int(self.metagraph.n / 25) - 1)):
+        print(attempt)
+        tasks = []
+        for i in range((attempt * limit), (attempt * limit) + limit):
+            # The dendrite client queries the network.
+            tasks.append(
+                check_uid_availability(
+                    dendrite,
+                    self.metagraph,
+                    uids[i],
+                    self.config.neuron.vpermit_tao_limit,
+                    None,
+                )
             )
-        )
-
-    responses = await asyncio.gather(*tasks)
+        responses += await asyncio.gather(*tasks)
+        attempt += 1
 
     for uid, uid_is_available in zip(range(self.metagraph.n.item()), (responses)):
         uid_is_not_excluded = exclude is None or uid not in exclude
