bbbf7c3b7490ea429491d7d4cdb61e27f7e5936b
kmfoda <kmfoda@gmail.com>
2025-06-27 13:11:24 +0100

    Update version

--- distributed_training/__init__.py
+++ distributed_training/__init__.py
@@ -17,7 +17,7 @@
 # DEALINGS IN THE SOFTWARE.
 
 # version nomenclature = __training_type__.__model__.__other_changes__
-__version__ = "1.1.4"
+__version__ = "1.1.5"
 __run__ = "5"
 version_split = __version__.split(".")
 __spec_version__ = (
